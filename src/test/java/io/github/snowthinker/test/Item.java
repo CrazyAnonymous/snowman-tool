d2562a9775c8cebc7591e3380e6ac004cfb37dea
Andrew <wj.andrew@yahoo.com>
2019-07-25 10:05:17 +0800

    update

---
+++ src/test/java/io/github/snowthinker/test/Item.java
@@ -0,0 +1,19 @@
+package io.github.snowthinker.test;
+
+import java.util.Date;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Item {
+
+	private Long id;
+	
+	private String name;
+	
+	private String sku;
+	
+	private Date createTime;
+}
